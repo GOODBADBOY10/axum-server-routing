2c46aca27fded92ca7cae4b23c09514d58bdd9ce
Goodbadboy <selfmanademola@gmail.com>
2025-10-06 22:36:30 +0100

    feat: setup my first axum project

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+#![allow(unused)] // for beginner only.
+use std::net::SocketAddr;
+
+use axum::Router;
+use axum::routing::get;
+use axum::response::Html;
+
+
+#[tokio::main]
+async fn main() {
+    let routes_hello = Router::new().route(
+        "/hello",
+        get(|| async {
+            Html("Hellow <strong>World!!!!</strong>")
+        })
+    );
+
+    //region: --- start server
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+    println!("->> LISTENING on {addr}\n");
+    axum::Server::bind(&addr)
+    .serve(routes_hello.into_make_service())
+    .await
+    .unwrap();
+
+    // end-region: ----- start serve
+}
