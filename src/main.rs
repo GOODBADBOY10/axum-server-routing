ff3615976d91b3534be79e11833c459a0b1ea263
Goodbadboy <selfmanademola@gmail.com>
2025-10-07 00:03:10 +0100

    feat: added a quick dev test file in the test folder

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,6 @@
 // use axum::routing::get;
 // use axum::response::Html;
 
-
 // #[tokio::main]
 // async fn main() {
 //     let routes_hello = Router::new().route(
@@ -26,27 +25,66 @@
 //     // end-region: ----- start serve
 // }
 
-
-
-#![allow(unused)] // for beginner only.
+#![allow(unused)]
 use std::net::SocketAddr;
 
 use axum::Router;
-use axum::routing::get;
 use axum::response::Html;
 use axum::response::IntoResponse;
-
+use axum::routing::get;
 
 #[tokio::main]
 async fn main() {
-    let routes_hello = Router::new().route(
-        "/hello",
-        get(handle_hello)
-    );
+    let routes_hello = Router::new().route("/hello", get(handler_hello));
 
-    // region: ----- Handler Hello
-    async fn handle_hello() -> impl IntoResponse {
-        println!("-->> {:<12} - handler_hello", "HANDLER");
-        Html("Hellow <strong>World!!!!</strong>")
-    }
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+    println!("Listening on http://{}", addr);
+    
+    // Axum 0.6 syntax
+    axum::Server::bind(&addr)
+        .serve(routes_hello.into_make_service())
+        .await
+        .unwrap();
 }
+
+async fn handler_hello() -> impl IntoResponse {
+    println!("-->> {:<12} - handler_hello", "HANDLER");
+    Html("Hello <strong>World!!!!</strong>")
+}
+
+
+
+
+
+
+
+
+// #![allow(unused)] // for beginner only.
+// use std::net::SocketAddr;
+
+// use axum::Router;
+// use axum::response::Html;
+// use axum::response::IntoResponse;
+// use axum::routing::get;
+
+// #[tokio::main]
+// async fn main() {
+//     let routes_hello = Router::new().route("/hello", get(handler_hello));
+
+//     // Add these lines to actually start the server
+//     let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+//     println!("Listening on http://{}", addr);
+    
+//     let listener = tokio::net::TcpListener::bind(addr)
+//         .await
+//         .unwrap();
+    
+//     axum::serve(listener, routes_hello.into_make_service())
+//         .await
+//         .unwrap();
+// }
+
+// async fn handler_hello() -> impl IntoResponse {
+//     println!("-->> {:<12} - handler_hello", "HANDLER");
+//     Html("Hello <strong>World!!!!</strong>")
+// }
