34b97e90636b9a6a6fdc91eb71661927f20810ee
Goodbadboy <selfmanademola@gmail.com>
2025-10-07 23:07:07 +0100

    feat: implement a quick dev test and also i refactored the main.rs file to make is use some axum function

--- src/main.rs
+++ src/main.rs
@@ -26,34 +26,84 @@
 // }
 
 #![allow(unused)]
+use tower_http::services::ServeDir;
 use std::net::SocketAddr;
-
+use axum::extract::Query;
+use axum::extract::Path;
 use axum::Router;
 use axum::response::Html;
 use axum::response::IntoResponse;
 use axum::routing::get;
+use axum::routing::get_service;
+use serde::Deserialize;
 
 #[tokio::main]
-async fn main() {
-    let routes_hello = Router::new().route("/hello", get(handler_hello));
+// async fn main() {
+//     let routes_hello = Router::new()
+//         .route("/hello", 
+//         get(handler_hello))
+//         .route("/hello2/:name", 
+//         get(handler_hello2));
 
+//     let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+//     println!("Listening on http://{}", addr);
+    
+//     // Axum 0.6 syntax
+//     axum::Server::bind(&addr)
+//         .serve(routes_hello.into_make_service())
+//         .await
+//         .unwrap();
+// }
+
+async fn main() {
+    let routes_all = Router::new()
+    .merge(routes_hello())
+    .fallback_service(routes_static());
+
+
+    // region ----- start server
     let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
     println!("Listening on http://{}", addr);
     
     // Axum 0.6 syntax
     axum::Server::bind(&addr)
-        .serve(routes_hello.into_make_service())
+        .serve(routes_all.into_make_service())
         .await
         .unwrap();
 }
 
-async fn handler_hello() -> impl IntoResponse {
-    println!("-->> {:<12} - handler_hello", "HANDLER");
-    Html("Hello <strong>World!!!!</strong>")
+
+fn routes_static() -> Router {
+    Router::new().nest_service("/", get_service(ServeDir::new("./")))
 }
 
 
+//region ----- Routes Hello--
+fn routes_hello() -> Router {
+    Router::new()
+        .route("/hello", get(handler_hello))
+        .route("/hello2/:name", get(handler_hello2))
+}
 
+
+#[derive(Debug, Deserialize)]
+struct HelloParams {
+    name: Option<String>,
+}
+
+async fn handler_hello(Query(params): Query<HelloParams>) -> impl IntoResponse {
+    println!("-->> {:<12} - handler_hello - {params:?}", "HANDLER");
+
+    let name = params.name.as_deref().unwrap_or("World");
+    Html(format!("Hello <strong>{name}</strong>"))
+}
+
+
+async fn handler_hello2(Path(name): Path<String>) -> impl IntoResponse {
+    println!("-->> {:<12} - handler_hello2 - {name:?}", "HANDLER");
+
+    Html(format!("Hello <strong>{name}</strong>"))
+}
 
 
 
